a229239a08fc022aa00312fe160ce7b8cadaf8e0
Dave2 Buchanan <146779710+unlox775-code-dot-org@users.noreply.github.com>
2025-08-27 16:42:24 -0700

    Removing whitespace changes

--- apps/whispering/src/lib/services/index.ts
+++ apps/whispering/src/lib/services/index.ts
@@ -5,7 +5,6 @@ import { DownloadServiceLive } from './download';
 import { FfmpegServiceLive } from './ffmpeg';
 import { GlobalShortcutManagerLive } from './global-shortcut-manager';
 import { LocalShortcutManagerLive } from './local-shortcut-manager';
-
 import { NotificationServiceLive } from './notifications';
 import { OsServiceLive } from './os';
 import { PermissionsServiceLive } from './permissions';
@@ -33,7 +32,6 @@ export {
 	FfmpegServiceLive as ffmpeg,
 	GlobalShortcutManagerLive as globalShortcutManager,
 	LocalShortcutManagerLive as localShortcutManager,
-
 	NotificationServiceLive as notification,
 	NativeRecorderServiceLive as nativeRecorder,
 	BrowserRecorderServiceLive as browserRecorder,
--- apps/whispering/src/lib/settings/settings.ts
+++ apps/whispering/src/lib/settings/settings.ts
@@ -94,8 +94,6 @@ export const settingsSchema = z.object({
 	'transformation.writeToCursorOnSuccess': z.boolean().default(false),
 
 	'system.alwaysOnTop': z.enum(ALWAYS_ON_TOP_VALUES).default('Never'),
-	
-
 
 	'database.recordingRetentionStrategy': z
 		.enum(['keep-forever', 'limit-count'])
